ef49e37ed0b3448eaf1312bae6c2e516825f1f27
mchocho99 <martinchocho99@gmail.com>
2026-03-16 12:40:30 -0300

    Se termina el ejercicio intermedio

--- zustand/intermedio/cart/src/components/Cart.tsx
+++ zustand/intermedio/cart/src/components/Cart.tsx
@@ -2,13 +2,23 @@ import useCartStore from '../store/useCartStore'
 import CartItem from './CartItem'
 
 const Cart = () => {
-  const { products } = useCartStore()
+  const { products, clear } = useCartStore()
+
+  const handleTotal = () => {
+    return products.reduce((acc, product) => acc + product.price * product.quantity, 0)
+  }
 
   return (
     <div>
-      {products &&
-        products.length > 0 &&
-        products.map((product) => <CartItem key={product.id} product={product} />)}
+      {products && products.length > 0 && (
+        <>
+          {products.map((product) => (
+            <CartItem key={product.id} product={product} />
+          ))}
+          <button onClick={clear}>Vaciar carrito</button>
+          <p>Total: {handleTotal()}</p>
+        </>
+      )}
     </div>
   )
 }
--- zustand/intermedio/cart/src/components/CartItem.tsx
+++ zustand/intermedio/cart/src/components/CartItem.tsx
@@ -2,10 +2,14 @@ import type { CartItemType } from '../types'
 import useCartStore from '../store/useCartStore'
 
 const CartItem = ({ product }: { product: CartItemType }) => {
-  const { updateQuantity } = useCartStore()
+  const { updateQuantity, deleteProduct } = useCartStore()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     updateQuantity(product.id, parseInt(e.target.value))
+  }
+
+  const handleDelete = () => {
+    deleteProduct(product.id)
   }
 
   return (
@@ -17,7 +21,9 @@ const CartItem = ({ product }: { product: CartItemType }) => {
         id="quantity"
         value={product.quantity}
         onChange={handleChange}
+        min={1}
       />
+      <button onClick={handleDelete}>Eliminar</button>
     </div>
   )
 }
